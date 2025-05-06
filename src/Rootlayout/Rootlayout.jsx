a3446ee041a50c4b2498cc9a029676fe0c9f5720
Mohammad oli56 <olimohammad286@gmai.com>
2025-05-06 22:18:33 +0600

    add json img

--- src/Rootlayout/Rootlayout.jsx
+++ src/Rootlayout/Rootlayout.jsx
@@ -21,10 +21,9 @@ const Rootlayout = () => {
     // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
-    // The signed-in user info.
+  
     const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
+    
   }).catch((error) => {
     // Handle Errors here.
     const errorCode = error.code;
