77f5176fb53da10ec1fc241685945734ce772d80
Mohammad oli56 <olimohammad286@gmai.com>
2025-05-07 18:23:53 +0600

    add loging ui

--- src/PrivateRoute/PrivateRut.jsx
+++ src/PrivateRoute/PrivateRut.jsx
@@ -2,15 +2,20 @@ import React, { useContext } from 'react';
 import { valueContext } from '../Rootlayout/Rootlayout';
 import { Navigate, useLocation } from 'react-router';
 
-const PrivateRut = ({children}) => {
+const PrivateRut = ({ children }) => {
 
-    const {userprofile,loding}= useContext(valueContext)
-    
+    const { userprofile, loding } = useContext(valueContext)
+
     const location = useLocation();
     console.log(location)
 
-    if(loding){
-        return<div>Loging.....</div>
+    if (loding) {
+        return <div className="flex flex-col items-center justify-center min-h-screen space-y-2">
+            <span className="loading loading-infinity loading-xl text-primary"></span>
+            <h1 className="text-lg font-semibold  text-primary">
+                Loading <span className="loading loading-dots loading-md text-primary"></span>
+            </h1>
+        </div>
     }
 
     if (!userprofile?.email) {
