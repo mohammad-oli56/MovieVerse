64c69741032c2044fcea5b325c3201ac9f88a09d
Mohammad oli56 <olimohammad286@gmai.com>
2025-05-08 17:46:50 +0600

    add slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,10 @@ import Movie from '../Component/movie';
 import More from '../Component/More';
 import Collspan from '../Component/Collspan';
 import { Helmet } from 'react-helmet-async';
+import img_1 from '../assets/original-98f50d5ce47e2deb898713ff0fe099e4.jpg'
+import img_2 from '../assets/1741900029595_SeedoftheSacredFig-27Mar-websiteslider.jpg'
+import img_3 from '../assets/dark-knight-rises-movie-poster-banner-catwoman.jpg'
+import img_4 from '../assets/fancy-slider.png'
 
 
 const Home = () => {
@@ -15,12 +19,12 @@ const Home = () => {
   const { subPlan, movies } = useLoaderData();
   // console.log(subPlan,movies) 
 
-  
+
 
   return (
 
     <>
-<Helmet>
+      <Helmet>
         <title>Home | MovieVerse</title>
         <meta name="description" content="Browse the best movies on MovieVerse" />
       </Helmet>
@@ -57,6 +61,39 @@ const Home = () => {
         <div className='max-w-screen-2xl  mx-auto px-8 mt-10 md:px-12 lg:px-16 '>
           <h1 className='text-purple-500 font-bold text-3xl mb-3.5'>Frequently Asked Questions</h1>
           <Collspan></Collspan>
+        </div>
+
+       
+        <h1 className="font-bold  mt-10 text-left text-3xl w-11/12 mx-auto text-purple-500">UpComing...</h1>
+        <div className=' bg-[radial-gradient(120%_160%_at_0%_100%,rgba(176,91,30,0.8)_0%,rgba(37,40,194,0.78)_95%)] p-3 w-11/12 mx-auto rounded-lg mt-4'>
+          
+          <div className="carousel w-full">
+            <div id="item1" className="carousel-item w-full">
+              <img
+                src={img_1}
+                className="w-full" />
+            </div>
+            <div id="item2" className="carousel-item w-full">
+              <img
+                src={img_2}
+                className="w-full" />
+            </div>
+            <div id="item3" className="carousel-item w-full">
+              <img
+                src={img_3}
+                className="w-full" />
+            </div>
+            <div id="item4" className="carousel-item w-full">
+              <img src={img_4} alt="" />
+            </div>
+          </div>
+          <div className="flex w-full justify-center gap-2 py-2">
+            <a href="#item1" className="btn btn-xs">1</a>
+            <a href="#item2" className="btn btn-xs">2</a>
+            <a href="#item3" className="btn btn-xs">3</a>
+            <a href="#item4" className="btn btn-xs">4</a>
+          </div>
+
         </div>
 
         <div className='h-12'>
